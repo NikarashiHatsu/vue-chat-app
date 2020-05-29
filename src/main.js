a5c2171722e5044eedb1c33297a97866fd775faf
NikarashiHatsu <yourlovelydev@gmail.com>
2020-05-29 16:59:45 +0700

    Importing Bootstrap, jQuery, and PopperJS to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/js/src/index'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
