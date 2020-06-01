b1c2ae2280bcebe09059bd94216409776fdb963c
NikarashiHatsu <yourlovelydev@gmail.com>
2020-06-01 23:59:48 +0700

    Added group chat functionality

--- src/components/chat/GroupChat.vue
+++ src/components/chat/GroupChat.vue
@@ -10,7 +10,36 @@
 </template>
 
 <script>
+  import firebase from 'firebase';
+
   export default {
-    name: 'Group'
+    name: 'Group',
+    data() {
+      return {
+        conversation: {
+          groupId: '',
+          chats: [],
+          sender: '',
+          text: ''
+        }
+      }
+    },
+
+    // TODO: MAKE AUTHENTICATED USER VARIABLE VALUE AS FIREBASE AUTHENTICATED USER
+    mounted() {
+      firebase.database().ref('chatgroup/' + this.$route.params.groupId).on('value', (snapshot) => {
+        var authenticatedUser = '';
+        var users = Object.entries(snapshot.val().users);
+        
+        users.forEach((key) => {
+          if(key[1] === authenticatedUser) {
+            this.conversation.groupId = snapshot.key;
+            this.conversation.chats = snapshot.val().messages.filter((e) => {
+              return e;
+            });
+          }
+        });
+      });
+    },
   }
 </script>
