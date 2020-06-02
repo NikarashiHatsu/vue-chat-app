612dd49e937a2b0123fbb87714ced45058afcd5b
NikarashiHatsu <yourlovelydev@gmail.com>
2020-06-03 01:44:46 +0700

    Revised todo

--- src/components/misc/Settings.vue
+++ src/components/misc/Settings.vue
@@ -3,12 +3,9 @@
   <!--
     TODO:
     1. All of the functionality
-    DONE:
-    1. User settings
-      b. Change user's profile picture
-      c. Change user's email address
-      d. Change user's password
-    2. Chat settings
+    2. User settings
+      a. Change user's profile picture
+    3. Chat settings
       a. Change theme from light to dark or vice-versa
   -->
   <div class="row justify-content-center">
